da9be42b2aa5e3d570a5898fa1d47405805e8bff
GuedaliaMahonAccount <gsebbah@g.jct.ac.il>
2025-05-11 13:45:24 +0300

    in english

--- unittests/renderer/ImageWriterTest.java
+++ unittests/renderer/ImageWriterTest.java
@@ -13,21 +13,21 @@ import renderer.ImageWriter;
 public class ImageWriterTest {
     @Test
     public void testWriteGrid() {
-        // יצירת מחלקת ImageWriter עם רזולוציה של 800x500
+        // Create an ImageWriter instance with a resolution of 800x500
         ImageWriter imageWriter = new ImageWriter(800, 500);
 
-        // צבעים לרקע ולרשת
+        // Set the background and grid colors
         Color backgroundColor = new Color(BLUE);
         Color gridColor = new Color(YELLOW);
 
-        // צביעת כל הפיקסלים לרקע
+        // Fill the entire image with the background color
         for (int y = 0; y < 500; y++) {
             for (int x = 0; x < 800; x++) {
                 imageWriter.writePixel(x, y, backgroundColor);
             }
         }
 
-        // הוספת רשת 16x10
+        // Add a 16x10 grid
         int gridSizeX = 50;
         int gridSizeY = 50;
         for (int y = 0; y < 500; y++) {
@@ -38,7 +38,7 @@ public class ImageWriterTest {
             }
         }
 
-        // כתיבת התמונה לקובץ
+        // Save the image to a file
         imageWriter.writeToImage("grid_test");
     }
 }
